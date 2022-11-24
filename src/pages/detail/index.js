7b8203010735ec5c91ff7de54ed2eef202d6c3a2
Heini-Maria Kotipelto <115211431+Heini-Maria@users.noreply.github.com>
2022-11-24 14:35:48 +0100

    finish tasks

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { getPetDetails } from '../../api/petfinder';
 import Hero from '../../components/hero';
-import { useParams } from 'react-router-dom';
+import { useParams, Redirect } from 'react-router-dom';
+import PetDetailsNotFound from '../petDetailsNotFound';
 
 const PetDetailsPage = () => {
   const [data, setData] = useState();
@@ -30,7 +31,9 @@ const PetDetailsPage = () => {
         <h3>Loading...</h3>
       ) : error ? (
         <div>
-          {/* Redirect to /pet-details-not-found if there was an error! */}
+          <Redirect to='/pet-details-not-found'>
+            <PetDetailsNotFound />
+          </Redirect>
         </div>
       ) : (
         <main>
